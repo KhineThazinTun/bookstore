ac38ccdb14d9debd75a14e5d11255146ff7e2628
KhineThazinTun <69158373+KhineThazinTun@users.noreply.github.com>
2020-08-04 00:31:53 +0630

    Add files via upload

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+	$(".del").click(function(e){
+        if(!confirm('Are you sure to delete?')){
+            e.preventDefault();
+            return false;
+        }
+        return true;
+    });
+});
+// -------------------------------
